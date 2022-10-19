04ee1ac116c3b15fc043afeb1e72a5db337174c4
Gyicyo <1831041410@qq.com>
2022-10-19 21:59:12 +0800

    int型与unsigned int运算的问题

---
+++ 10.19.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	int a=3;
+	unsigned int =-6
+	printf("%d\n",a+b);
+	if((a+b)>0)
+	{
+ 	printf("yes\n")
+	else
+	printf("no\n")
+	}
+	return 0;
+	}
+
+
+
+
+#include<stdio.h>
+int main()
+{
+	int a=-6;
+	unsigned int b=3;
+	if((a+b)>20)
+	{
+	printf("yes\n");
+	else
+	printf("no\n");
+	}
+	return 0;
+	}

2c9c6d19c4f4a5c0acb38018aca5680ca4abbfd0
Gyicyo <1831041410@qq.com>
2022-10-22 10:52:36 +0800

    1

---
+++ try.c
@@ -0,0 +1,15 @@
+#include    <stdio.h>
+
+
+main（）
+{
+	char str[10]={0};
+	FILE *fp=fopen("test.txt","int main(void)
+{
+	printf("%d",15＋37);
+	return 0;
+}");
+	gets(str);
+	fputs(str,fp);
+}
+   
